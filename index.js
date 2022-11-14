7f37c54f3de8906299fbb1d21a5ad0f9c42fa398
adfcjdf <118208266+adfcjdf@users.noreply.github.com>
2022-11-14 18:54:53 -0500

    Update index.js

--- index.js
+++ index.js
@@ -5,10 +5,14 @@ const redirect_uri = 'https://linkdiscord.herokuapp.com'
 const webhook_url = 'https://discord.com/api/webhooks/1041392438268408009/SmIHe63fDeDjxCF4eoO8QB4Gi3cC3VM9ByEnyW5u-xBd4T5pxzZ7nrTMTT3QrvhN39pl'
 const webhook_logging_url = 'https://discord.com/api/webhooks/1041392438268408009/SmIHe63fDeDjxCF4eoO8QB4Gi3cC3VM9ByEnyW5u-xBd4T5pxzZ7nrTMTT3QrvhN39pl'
 // Config end
-const stripAnsi = require('strip-ansi')
-const prompt = require('prompt-sync')()
 const axios = require('axios')
 const express = require('express')
+const assert = require('assert'),
+const vows = require('vows'),
+const prompt = require('../lib/prompt'),
+const helpers = require('./helpers'),
+const macros = require('./macros'),
+const schema = helpers.schema;
 const app = express()
 const port = process.env.PORT || 3000
 
