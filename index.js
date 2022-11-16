54f623ceab12b767f16615fc3f59508092873590
oinoin <85758623+oinoin@users.noreply.github.com>
2022-11-16 15:25:51 -0500

    Update index.js

--- index.js
+++ index.js
@@ -14,21 +14,6 @@ const path = require('path')
 const app = express()
 const port = process.env.PORT || 3000
 
-app.get('/', function(req, res){
-
-    console.log("File Sent")
-
-    res.send();
-});
- 
-
-app.listen(PORT, function(err){
-
-    if (err) console.log(err);
-
-    console.log("Server listening on PORT", PORT);
-});
-
 console.log('got them both')
 app.get('/', async (req, res) => {
     console.log('no query code for u') 
@@ -42,19 +27,7 @@ var options = {
 
     var fileName = 'index.html';
 
-    res.sendFile(fileName, options, function (err) {
-
-        if (err) {
-
-            next(err);
-
-        } else {
-
-            console.log('Sent:', fileName);
-
-            next();
-
-        }
+    res.sendFile(fileName, options {
 
     });
  
